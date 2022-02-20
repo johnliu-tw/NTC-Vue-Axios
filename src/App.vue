380360ae4982d326cd0a4e332fc88e3d282ab8ce
johnliu <johnliutw@hotmail.com>
2022-02-20 15:24:50 +0800

    add user link list

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <li v-for=" i in 10" :key="i">
+      <router-link :to="`/users/${i}`">使用者: {{ i }}</router-link>
+    </li>
   </nav>
   <router-view/>
 </template>
