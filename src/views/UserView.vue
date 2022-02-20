9dd3d4ed7440f920570af9e6770c1069a8672a42
johnliu <johnliutw@hotmail.com>
2022-02-20 15:25:04 +0800

    add UserView

---
+++ src/views/UserView.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>UserId: {{ $route.params.userId }}</h1>
+  <div>Name: {{ userInfo.name }}</div>
+  <div>Email: {{ userInfo.email }}</div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  data(){
+    return{
+      userInfo:{}
+    }
+  },
+  mounted(){
+    axios.get(`https://jsonplaceholder.typicode.com/users/${this.$route.params.userId}`)
+         .then(response => this.userInfo = response.data)
+  }
+}
+</script>
